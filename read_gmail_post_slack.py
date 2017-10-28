aa08e9f15867a41fb55da7d6260710ef7b9c6130
motomk <moto.rx78@gmail.com>
2017-10-28 17:07:31 +0900

    post slack

--- read_gmail_post_slack.py
+++ read_gmail_post_slack.py
@@ -1,8 +1,12 @@
+# coding: utf-8
 from __future__ import print_function
 import httplib2
 import os
 import base64
 import email
+import datetime
+import subprocess
+from config.config_data import *
 
 from apiclient import discovery
 from oauth2client import client
@@ -11,27 +15,16 @@ from oauth2client.file import Storage
 
 try:
     import argparse
-
     flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
 except ImportError:
     flags = None
 
-# If modifying these scopes, delete your previously saved credentials
-# at ~/.credentials/gmail-python-quickstart.json
 SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Gmail API Python Quickstart'
 
 
 def get_credentials():
-    """Gets valid user credentials from storage.
-
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
-
-    Returns:
-        Credentials, the obtained credential.
-    """
     home_dir = os.path.expanduser('~')
     credential_dir = os.path.join(home_dir, '.credentials')
     if not os.path.exists(credential_dir):
@@ -46,22 +39,61 @@ def get_credentials():
         flow.user_agent = APPLICATION_NAME
         if flags:
             credentials = tools.run_flow(flow, store, flags)
-        else:  # Needed only for compatibility with Python 2.6
+        else:
             credentials = tools.run(flow, store)
         print('Storing credentials to ' + credential_path)
     return credentials
 
+
+def get_from(mime_msg):
+    from_str = email.header.decode_header(mime_msg.get('From'))
+    addr = ""
+    for f in from_str:
+        if isinstance(f[0], bytes):
+            addr += f[0].decode('utf-8')
+        else:
+            addr += f[0]
+    return addr
+
+def get_subject(mime_msg):
+    subject = email.header.decode_header(mime_msg.get('Subject'))
+    # print(subject)
+    title = ""
+    for sub in subject:
+        if isinstance(sub[0], bytes):
+            title = sub[0].decode(sub[1])
+        else:
+            title = sub[0]
+    return title
+
+def get_body(mime_msg):
+    for part in mime_msg.walk():
+        if part.get_content_maintype() == 'multipart':
+            continue
+
+        body = ""
+        charset = str(part.get_content_charset())
+        if charset:
+            tmp = unicode(part.get_payload(), charset)
+            for line in tmp.split('\n'):
+                if SEARCH_INDEX_STR in line:
+                    body += line
+    return body
+
+def send_slack(msg):
+
+    send_str =  "curl -X POST -H 'Content-type: application/json' --data '{\"text\":\"" + msg + "\"}' " + SLACK_WEBHOOK_URL
+    print(send_str)
+    subprocess.call(send_str, shell=True)
 
 def main():
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('gmail', 'v1', http=http)
 
-    # results = service.users().labels().list(userId='me').execute()
-    # labels = results.get('labels', [])
-
-
-    results = service.users().messages().list(userId='me', q="").execute()
+    q_time = datetime.datetime.today() + datetime.timedelta(minutes=MINUTES)
+    q_str = QSTRING + " after:" + q_time.strftime('%s')
+    results = service.users().messages().list(userId='me', q=q_str).execute()
     messages = results.get('messages', [])
 
     if not messages:
@@ -75,42 +107,25 @@ def main():
             msg_str = base64.urlsafe_b64decode(msg_raw['raw'].encode('ASCII'))
 
             mime_msg = email.message_from_string(msg_str)
-            print(mime_msg.keys())
+            # print(mime_msg.keys())
 
             # from
-            form_str = email.header.decode_header(mime_msg.get('From'))
-            addr = ""
-            for f in form_str:
-                if isinstance(f[0], bytes):
-                    addr += f[0].decode('utf-8')
-                else:
-                    addr += f[0]
+            addr = get_from(mime_msg)
+            if addr != CHECK_ADDRESS:
+                continue
             print(addr)
 
             # subject
-            subject = email.header.decode_header(mime_msg.get('Subject'))
-            # print(subject)
-            title = ""
-            for sub in subject:
-                if isinstance(sub[0], bytes):
-                    title = sub[0].decode(sub[1])
-                else:
-                    title = sub[0]
-            print(title)
+            title = get_subject(mime_msg)
+            if title != CHECK_SUBJECT:
+                continue
+            else:
+                print(title)
 
-            # body
-            for part in mime_msg.walk():
-                if part.get_content_maintype() == 'multipart':
-                    continue
-
-                body = ""
-                charset = str(part.get_content_charset())
-                if charset:
-                    body = unicode(part.get_payload(), charset)
-
-                print(body)
+            body = get_body(mime_msg)
+            send_slack(body)
+            print(body)
 
 
-            print("-------------------")
 if __name__ == '__main__':
     main()
